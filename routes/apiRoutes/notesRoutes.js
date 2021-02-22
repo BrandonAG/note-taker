e69e881942bc355ee5d4e2ddf8434270757e6159
Brandon <brandon.eg@gmail.com>
2021-02-21 23:09:44 -0800

    add and delete notes

---
+++ routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+const {
+    findById,
+    createNewNote,
+    deleteNote,
+    validateNote,
+  } = require("../../lib/notes");
+const { notes } = require("../../db/db")
+
+router.get("/notes", (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+
+router.get("/notes/:id", (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+router.post("/notes", (req, res) => {
+    req.body.id = notes.length.toString();
+    
+    if (!validateNote(req.body)) {
+        res.status(400).send("Entry fields are empty.");
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+router.delete("/notes/:id", (req, res) => {
+    let id = req.params.id;
+    // notes.filter((note) => note.id !== id);
+    deleteNote(id, notes)
+    res.json(notes);
+});
+
+module.exports = router;
